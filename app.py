c92c0283d2565d4e6d02647d72edeee5185d3f98
shivamkapoor172002 <92868323+shivamkapoor172002@users.noreply.github.com>
2023-06-28 18:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,93 +1,74 @@
-import streamlit as st
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from transformers import pipeline
 import torch
-import base64
-import textwrap
-from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain.llms import HuggingFacePipeline
-from constants import CHROMA_SETTINGS
-from streamlit_chat import message
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
-checkpoint = "LaMini-T5-738M"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-base_model = AutoModelForSeq2SeqLM.from_pretrained(
-    checkpoint,
+# Load the model and tokenizer
+model_name = "import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+# Load the model and tokenizer
+model_name = "tiiuae/falcon-7b"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# Create a text generation pipeline
+text_generator = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
     device_map="auto",
-    torch_dtype=torch.float32
 )
 
-@st.cache(allow_output_mutation=True)
-def llm_pipeline():
-    pipe = pipeline(
-        'text2text-generation',
-        model=base_model,
-        tokenizer=tokenizer,
-        max_length=256,
+
+def generate_text(prompt, max_length=200, top_k=10, num_return_sequences=1):
+    sequences = text_generator(
+        prompt,
+        max_length=max_length,
         do_sample=True,
-        temperature=0.3,
-        top_p=0.95
+        top_k=top_k,
+        num_return_sequences=num_return_sequences,
+        eos_token_id=tokenizer.eos_token_id,
     )
-    local_llm = HuggingFacePipeline(pipeline=pipe)
-    return local_llm
+    return sequences[0]["generated_text"]
 
-@st.cache(allow_output_mutation=True)
-def qa_llm():
-    llm = llm_pipeline()
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    db = Chroma(persist_directory="db", embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever()
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        return_source_documents=True
+
+# Local app
+prompt = input("Enter the prompt: ")
+generated_text = generate_text(prompt)
+
+print("\nGenerated Text:")
+print(generated_text)
+"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# Create a text generation pipeline
+text_generator = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+
+
+def generate_text(prompt, max_length=200, top_k=10, num_return_sequences=1):
+    sequences = text_generator(
+        prompt,
+        max_length=max_length,
+        do_sample=True,
+        top_k=top_k,
+        num_return_sequences=num_return_sequences,
+        eos_token_id=tokenizer.eos_token_id,
     )
-    return qa
+    return sequences[0]["generated_text"]
 
-def process_answer(instruction):
-    response = ''
-    instruction = instruction['query']
-    qa = qa_llm()
-    generated_text = qa(instruction)
-    answer = generated_text['result']
-    return answer
 
-# Display conversation history using Streamlit messages
-def display_conversation(history):
-    for i in range(len(history["generated"])):
-        message(history["past"][i], is_user=True, key=str(i) + "_user")
-        message(history["generated"][i], key=str(i))
+# Local app
+prompt = input("Enter the prompt: ")
+generated_text = generate_text(prompt)
 
-def main():
-    st.title('Chat with Your Data 🦜📄')
-    with st.expander("About the Chatbot"):
-        st.markdown(
-            """
-            This is a Generative AI powered Chatbot that interacts with you and you can ask followup questions.
-            """
-        )
-
-    user_input = st.text_input("", key="input")
-
-    # Initialize session state for generated responses and past messages
-    if "generated" not in st.session_state:
-        st.session_state["generated"] = ["I am ready to help you"]
-    if "past" not in st.session_state:
-        st.session_state["past"] = ["Hey there!"]
-
-    # Search the database for a response based on user input and update session state
-    if user_input:
-        answer = process_answer({'query': user_input})
-        st.session_state["past"].append(user_input)
-        response = answer
-        st.session_state["generated"].append(response)
-
-    # Display conversation history using Streamlit messages
-    if st.session_state["generated"]:
-        display_conversation(st.session_state)
-
-if __name__ == '__main__':
-    main()
+print("\nGenerated Text:")
+print(generated_text)
